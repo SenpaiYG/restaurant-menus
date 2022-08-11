069a0d9975653a6bfa4c90bfc4ef705e86406bcb
SenpaiYG <yaxye_yssss@hotmail.com>
2022-08-11 11:15:43 +0100

    somemessage

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const {sequelize} = require('./db')
+const {db} = require('./db')
 const {Restaurant, Menu} = require('./models/index')
 const {
     seedRestaurant,
@@ -13,12 +13,20 @@ describe('Restaurant and Menu Models', () => {
         // the 'sync' method will create tables based on the model class
         // by setting 'force:true' the tables are recreated each time the 
         // test suite is run
-        await sequelize.sync({ force: true });
+        await db.sync({ force: true });
     });
 
     test('can create a Restaurant', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+
+        // const testRestaurant = await Restaurant.create({
+        //     name: 'KFC',
+        //     location: 'London',
+        //     cuisine: 'American'
+        // })
+
+        const seedTestRestaurant = await Restaurant.create(seedRestaurant[0])
+
+        expect(seedTestRestaurant.name).toEqual(seedRestaurant[0].name)
     });
 
     test('can create a Menu', async () => {
@@ -36,8 +44,8 @@ describe('Restaurant and Menu Models', () => {
         expect('NO TEST').toEqual('EXPECTED DATA')
     });
 
-    test('can delete Restaurants', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
+    // test('can delete Restaurants', async () => {
+    //     // TODO - write test
+    //     expect('NO TEST').toEqual('EXPECTED DATA')
+    // });
 })
--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,6 +1,28 @@
-const {sequelize} = require('../db');
-const { Sequelize } = require('sequelize');
+const {db} = require('../db');
+const { Sequelize, DataTypes } = require('sequelize');
 
-// TODO - create a Restaurant model
+const Restaurant = db.define('restaurant', {
+    name: {
+        type: DataTypes.STRING
+    },
+
+    location: {
+        type: DataTypes.STRING
+    },
+    
+    cuisine: {
+        type: DataTypes.STRING
+    },
+})
+async function main () {
+    await Restaurant.sync({ force: true })
+    
+    await Restaurant.create({
+        name: 'KFC',
+        location: 'London',
+        cuisine: 'American'
+    })
+}
+// main()
 
 module.exports = {Restaurant};
